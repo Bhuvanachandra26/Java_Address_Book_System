c05ea9bf4d933c1abf7df81b60e6ea36c9ca7889
Bhuvanachandra26 <bhuvanachandra26@gmail.com>
2021-12-14 20:20:36 +0530

    Added: code to read and write the address book into file using file IO

--- src/com/bridgelabz/addressbook/AddressBookOperations.java
+++ src/com/bridgelabz/addressbook/AddressBookOperations.java
@@ -10,13 +10,13 @@ public class AddressBookOperations {
     static String f_name;
     static Scanner s=new Scanner(System.in);
 
-    public static void addAddressBook(Map<String, List<Contact>> addressBooks) {
+    public static void addAddressBook(Map<String, List<ContactAddress>> addressBooks) {
         System.out.println("Add new AddressBook");
         String bookName = s.next();
-        addressBooks.put(bookName, new ArrayList<Contact>());
+        addressBooks.put(bookName, new ArrayList<ContactAddress>());
     }
 
-    public static void displayAllAddressBook(Map<String, List<Contact>> addressBooks) {
+    public static void displayAllAddressBook(Map<String, List<ContactAddress>> addressBooks) {
         boolean is_Empty=addressBooks.isEmpty();
         if(is_Empty==true)
             System.out.println("No Address Books are Present");
@@ -24,12 +24,12 @@ public class AddressBookOperations {
             System.out.println(addressBooks);
     }
 
-    public static void editAddressBook(Map<String, List<Contact>> addressBooks) {
+    public static void editAddressBook(Map<String, List<ContactAddress>> addressBooks) {
         System.out.println("Enter the name of the AddressBook you want to Edit");
         String bookName1 = s.next();
 
         if(addressBooks.containsKey(bookName1)) {
-            List<Contact> contact = addressBooks.get(bookName1);
+            List<ContactAddress> contact = addressBooks.get(bookName1);
             int choice;
             do{
                 System.out.println("Enter your option : ");
@@ -41,6 +41,10 @@ public class AddressBookOperations {
                 System.out.println("6.View Contact's First Name and Last Name by City ");
                 System.out.println("7.Count of Total Contacts By City");
                 System.out.println("8.Count of Total Contacts By State");
+                System.out.println("9.Sort Contacts by first Name");
+                System.out.println("10.Sorted Contacts by City");
+                System.out.println("11.Sorted Contacts by State");
+                System.out.println("12.Sorted Contacts by Zip");
 
                 choice=s.nextInt();
                 switch (choice)
@@ -76,6 +80,18 @@ public class AddressBookOperations {
                         break;
                     case 8:
                         ContactOperations.countByState(contact);
+                        break;
+                    case 9:
+                        ContactOperations.sortedContactByFirstName(contact);
+                        break;
+                    case 10:
+                        ContactOperations.sortedContactByCity(contact);
+                        break;
+                    case 11:
+                        ContactOperations.sortedContactByState(contact);
+                        break;
+                    case 12:
+                        ContactOperations.sortedContactByZip(contact);
                         break;
                     default:
                         System.out.println("Enter valid option");
