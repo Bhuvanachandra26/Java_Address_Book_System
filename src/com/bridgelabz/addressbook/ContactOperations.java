f98a2ae0df92ca6c9176c68109416f364ddc7f6c
Bhuvanachandra26 <bhuvanachandra26@gmail.com>
2021-12-14 20:22:52 +0530

    Added: code to read and write the address book into file using file IO

--- src/com/bridgelabz/addressbook/ContactOperations.java
+++ src/com/bridgelabz/addressbook/ContactOperations.java
@@ -8,8 +8,8 @@ import java.util.stream.Collectors;
 public class ContactOperations {
     static Scanner s=new Scanner(System.in);
 
-    public static void Add_Contact(String fString,List<Contact> contact) {
-        Contact c=new Contact();
+    public static void Add_Contact(String fString,List<ContactAddress> contact) {
+        ContactAddress c=new ContactAddress();
 
         c.setfirstName(fString);
         System.out.print("Enter Last Name : ");
@@ -28,8 +28,8 @@ public class ContactOperations {
 
     }
 
-    public static boolean checkDuplicate(String f_name,List<Contact> contact) {
-        for (Contact c : contact) {
+    public static boolean checkDuplicate(String f_name,List<ContactAddress> contact) {
+        for (ContactAddress c : contact) {
             if (c.getfirstName().equals(f_name)) {
                 return true;
             }
@@ -38,7 +38,7 @@ public class ContactOperations {
     }
 
 
-    public static void Display_All(List<Contact> contact){
+    public static void Display_All(List<ContactAddress> contact){
         boolean is_Empty=contact.isEmpty();
         if(is_Empty==true)
             System.out.println("Array List is Empty");
@@ -46,13 +46,13 @@ public class ContactOperations {
             System.out.println(contact);
     }
 
-    public static void editContact(List<Contact> contact ) {
+    public static void editContact(List<ContactAddress> contact ) {
         System.out.println("Enter first name that you want to Edit:");
         String firstName = s.next();
 
         for (int i = 0; i < contact.size(); i++) {
             if (contact.get(i).getfirstName().equalsIgnoreCase(firstName)) {
-                Contact c = contact.get(i);
+                ContactAddress c = contact.get(i);
                 System.out.print("Enter new First Name: ");
                 c.setfirstName(s.next());
                 System.out.print("Enter  new Last Name : ");
@@ -76,7 +76,7 @@ public class ContactOperations {
         }
     }
 
-    public static void deleteContact(List<Contact> contact){
+    public static void deleteContact(List<ContactAddress> contact){
         System.out.println("Enter first name that you want to Delete:");
         String firstName = s.next();
         for (int i = 0; i < contact.size(); i++) {
@@ -88,19 +88,19 @@ public class ContactOperations {
         }
     }
 
-    public static void searchByCityOrState(List<Contact> contact) {
+    public static void searchByCityOrState(List<ContactAddress> contact) {
         System.out.println("Enter CityName: ");
         String city = s.next();
         contact.stream().filter(c -> c.getcity().equals(city)).forEach(System.out::println);
     }
 
-    public static void viewPersonByCityOrState(List<Contact> contact) {
+    public static void viewPersonByCityOrState(List<ContactAddress> contact) {
         System.out.println("Enter CityName: ");
         String city = s.next();
         contact.stream().filter(c -> c.getcity().equals(city)).forEach(cn -> System.out.println("First Name : "+cn.getfirstName()+"  Last Name : "+cn.getlastName()));
     }
 
-    public static void countByCity(List<Contact> contact) {
+    public static void countByCity(List<ContactAddress> contact) {
 
         System.out.println("Enter the name of the city:");
         String city=s.next();
@@ -109,11 +109,70 @@ public class ContactOperations {
 
     }
 
-    public static void countByState(List<Contact> contact) {
+    public static void countByState(List<ContactAddress> contact) {
 
         System.out.println("Enter the name of the State:");
         String state=s.next();
         Long countNamesByState=contact.stream().filter(e -> state.equals(e.getstate())).count();
         System.out.println(state+" : "+countNamesByState);
     }
+
+    public static void sortedContactByFirstName(List<ContactAddress> contact) {
+
+        List<ContactAddress> sortedContact=contact.stream().sorted(new compareFirstName()).collect(Collectors.toList());
+        System.out.println(sortedContact);
+    }
+
+
+    public static void sortedContactByCity(List<ContactAddress> contact) {
+        contact.stream().sorted(new compareCity()).forEach(System.out::println);
+
+    }
+
+    public static void sortedContactByState(List<ContactAddress> contact) {
+        contact.stream().sorted(new compareState()).forEach(System.out::println);
+
+    }
+
+    public static void sortedContactByZip(List<ContactAddress> contact) {
+        contact.stream().sorted(new compareZip()).forEach(System.out::println);
+
+    }
+}
+
+class compareFirstName implements Comparator<ContactAddress> {
+
+    @Override
+    public int compare(ContactAddress o1, ContactAddress o2) {
+
+        return o1.getfirstName().compareTo(o2.getfirstName());
+    }
+
+}
+
+class compareCity implements Comparator<ContactAddress> {
+
+    @Override
+    public int compare(ContactAddress o1, ContactAddress o2) {
+        return o1.getcity().compareTo(o2.getcity());
+    }
+
+}
+
+class compareState implements Comparator<ContactAddress> {
+
+    @Override
+    public int compare(ContactAddress o1, ContactAddress o2) {
+        return o1.getstate().compareTo(o2.getstate());
+    }
+
+}
+
+class compareZip implements Comparator<ContactAddress> {
+
+    @Override
+    public int compare(ContactAddress o1, ContactAddress o2) {
+        return o1.getzip().compareTo(o2.getzip());
+    }
+
 }
